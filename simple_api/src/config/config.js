4bdb0bc10a8d77bb12c01569631929780beea4f7
calebeLAR <ca6ele@gmail.com>
2023-04-07 10:37:31 -0400

    exercicio01: Crie uma migration para criar uma tabela books

--- simple_api/src/config/config.js
+++ simple_api/src/config/config.js
@@ -1,10 +1,9 @@
-// src/config/config.js
-
 const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'senha',
+  database: process.env.MYSQL_DATABASE || 'simpleAPI',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.PORT || 3000,
   dialect: 'mysql',
 };
 
