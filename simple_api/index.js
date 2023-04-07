61a4320db1ac912c128dfe5dc78315fb9d26af3d
calebeLAR <ca6ele@gmail.com>
2023-04-07 11:04:11 -0400

    Exercício 5: Vincule a rota GET /books para acessar seu controller.

--- simple_api/index.js
+++ simple_api/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 console.log(PORT)
 
 app.use(express.json());
-
+// ex05
 app.get('/books', BooksController.getAll)
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
