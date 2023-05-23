7f88b2280d8eff5603a899b535ac68ca35ccf1ce
calebeLAR <ca6ele@gmail.com>
2023-05-23 09:08:18 -0400

    remove

--- exercise02/src/authors.ts
+++
@@ -1,9 +0,0 @@
-import Author from './database/models/Author';
-import Book from './database/models/Book';
-
-(async () => {
-  const authors = await Author.findAll();
-  console.table(authors.map((author) => author.toJSON()));
-  
-  process.exit(0);
-})();
